117c84bee30635faeecd1a94a2275d1d2ffe31b8
gavinsh32 <gavinhaynes32@gmail.com>
2025-05-31 12:32:12 -0700

    Added copy functions

--- tree.py
+++ tree.py
@@ -4,7 +4,7 @@ from enum import Enum
 from typing import Union, TypeAlias
 
 def main():
-    ex_indiv = Individual(5)
+    ex_indiv = Individual(4)
     inputs = [_generate_value() for _ in range(NUM_VARS)]
     print('Inputs:')
     print(inputs)
@@ -24,6 +24,13 @@ class Operator():
     
     def get(self) -> int:
         return self.kind
+    
+    def copy(self) -> 'Operator':
+        dst = Operator()
+        dst.kind = self.kind
+        dst.left_child = self.left_child
+        dst.right_child = self.right_child
+        return dst
     
     def __str__(self):
         match self.kind:
@@ -52,6 +59,11 @@ class Variable():
 
     def __str__(self):
         return f'x{self.id}'
+    
+    def copy(self) -> 'Variable':
+        dst = Variable()
+        dst.id = self.id
+        return dst
 
 CONST_RANGE = 2.0
 
@@ -65,6 +77,11 @@ class Constant():
 
     def __str__(self):
         return f'{self.data}'
+    
+    def copy(self) -> 'Constant':
+        dst = Constant()
+        dst.data = self.data
+        return dst
 
 Node = Operator
 Terminal: TypeAlias = Union[Constant, Variable]
